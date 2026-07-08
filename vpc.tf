821a726b81db45e240813015e02cdaf3cf752d48
nurbaitenov <baitenovnur@gmail.com>
2026-07-08 10:42:37 +0000

    added

--- vpc.tf
+++ vpc.tf
@@ -46,7 +46,7 @@ resource "aws_eip" "nat" {
 
 
 
-resource "aws_route_table" "example" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -55,16 +55,35 @@ resource "aws_route_table" "example" {
   }
 
   tags = {
-    Name = "example"
+    Name = "public"
   }
 }
 
 resource "aws_route_table_association" "public1" {
   subnet_id      = aws_subnet.public1.id
-  route_table_id = aws_route_table.example.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public2" {
   subnet_id      = aws_subnet.public2.id
-  route_table_id = aws_route_table.example.id
+  route_table_id = aws_route_table.public.id
 }
+
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.main.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.example.id   # Private Route table attaches to NAT Gateway
+#   }
+
+#   tags = {
+#     Name = "private"
+#   }
+# }
+
+# resource "aws_route_table_association" "private1" {
+#   subnet_id      = aws_subnet.private1.id
+#   route_table_id = aws_route_table.private.id
+# }
+
